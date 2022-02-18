c1d34b65905b3f9afb0c0f01950c34f333835be3
Alex Anderson <ama7019@psu.edu>
2022-02-18 12:52:27 -0500

    Test.txt needs correct outputs for each test case

--- src/Foo.java
+++ src/Foo.java
@@ -1,19 +1,25 @@
 import java.io.File;
+import java.io.FileWriter;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Foo {
     Scanner scanner;
-    File file;
+    File inputFile, outputFile;
+    FileWriter writer;
     int firstValue, secondValue, testCaseNumber = 0, sum = 0;
 
 
     Foo() {
-        File file = new File("src/Eq.txt");
-        try {
-            scanner = new Scanner(file);
-        } catch (Exception e) {
+        inputFile = new File("src/Eq.txt");
+        outputFile = new File("src/test.txt");
 
+        try {
+            scanner = new Scanner(inputFile);
+            outputFile.createNewFile();
+            writer = new FileWriter(outputFile);
+        } catch (Exception e) {
+            System.out.println("An error has occurred");
         }
     }
 
@@ -25,31 +31,53 @@ public class Foo {
             if (nextValue.endsWith(",")) {
                 nextValue = nextValue.substring(0, nextValue.length() - 1);
                 firstValue = Integer.parseInt(nextValue);
+                if(testCaseNumber == 0) {
+                    String valueString = firstValue - 1 + " ";
+                    try {
+                        writer.append(valueString);
+                    } catch (Exception e) {
+                        System.out.println("Can't write to file");
+                    }
+                }
             } else if (nextValue.endsWith(";")) {
                 nextValue = nextValue.substring(0, nextValue.length() - 1);
                 secondValue = Integer.parseInt(nextValue);
                 testCaseNumber++;
+                try {
+                    writer.append(firstValue + " " + (firstValue + 1) + " " + secondValue + " ");
+                } catch (Exception e) {
+                    System.out.println("Can't write to file");
+                }
                 if (this.check(n) > 0) {
                     fooSum += this.check(n);
                 }
             } else {
                 secondValue = Integer.parseInt(nextValue);
                 testCaseNumber++;
-                if (this.check(n) > 0) {
-                    return this.check(n) + foo(random.nextInt(30) + 1);
-                }
+                fooSum += this.check(n);
                 testCaseNumber = 0;
+                try {
+                    writer.append(firstValue + " " + (firstValue + 1) + " " + secondValue + " " + (secondValue + 1) + "\n");
+                } catch (Exception e) {
+                    System.out.println("Can't write to file");
+                }
+                return fooSum + foo(random.nextInt(30) + 1);
             }
         }
+        try {
+            writer.close();
+        } catch (Exception e) {
+            System.out.println("Could not close file");
+        }
 
-        return -1;
+        return fooSum;
     }
 
     private int check(int val) {
         if (val >= firstValue && val <= secondValue) {
             return testCaseNumber;
         } else {
-            return -1;
+            return 0;
         }
     }
 
@@ -57,12 +85,8 @@ public class Foo {
         Random random = new Random();
         Foo foo = new Foo();
         int num = random.nextInt(30) + 1;
-        System.out.println(num);
         System.out.println(foo.foo(num));
-        foo.resetSum();
-        foo.resetScanner();
-        System.out.println(foo.fooOld(num));
-        System.out.println("I swear this works");
+
     }
 
     public int fooOld(int n) {
@@ -93,15 +117,4 @@ public class Foo {
         return sum;
     }
 
-    public void resetSum() {
-        sum = 0;
-    }
-
-    public void resetScanner() {
-        try {
-            scanner = new Scanner(file);
-        } catch (Exception e) {
-
-        }
-    }
 }
